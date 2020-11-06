6595b7c22ca646483b91b23146a12637419dec5b
Madusanka <madushankanipunajith@gmail.com>
2020-11-06 17:48:06 +0530

    11/06/2020

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,11 +5,11 @@
         	<?php
         		$uri= $_SERVER['REQUEST_URI']; 
         	?>
-    <li><i class="fa fa-home" aria-hidden="true"></i><a href="localhost/bloodbank/admin/index" <?php if (strpos($uri, '/admin/index')){ echo "class= \"active\" "; }?> >Home</a></li>
-    <li><i class="fa fa-user-md" aria-hidden="true"></i><a href="localhost/bloodbank/admin/manage_admin/index"<?php if (strpos($uri, '/admin/manage_admin')){ echo "class= \"active\" "; }?> >Manage Admin</a></li>
- 	<li><i class="fa fa-hospital-o" aria-hidden="true"></i><a href="localhost/bloodbank/admin/manage_hospital/index" <?php if (strpos($uri, '/admin/manage_hospital')){ echo "class= \"active\" "; }?> >Manage Hospitals</a></li>
-    <li><i class="fa fa-file-text" aria-hidden="true"></i><a href="localhost/bloodbank/admin/view_report/index" <?php if (strpos($uri, '/admin/view_report')){ echo "class= \"active\" "; }?> >View Report</a></li>
-    <li><i class="fa fa-cogs" aria-hidden="true"></i><a href="localhost/bloodbank/admin/profile/index" <?php if (strpos($uri, '/admin/profile')){ echo "class= \"active\" "; }?> >Settings</a></li>
+    <li><i class="fa fa-home" aria-hidden="true"></i><a href="/bloodbank/admin/index" <?php if (strpos($uri, '/admin/index')){ echo "class= \"active\" "; }?> >Home</a></li>
+    <li><i class="fa fa-user-md" aria-hidden="true"></i><a href="/bloodbank/admin/manage_admin/index"<?php if (strpos($uri, '/admin/manage_admin')){ echo "class= \"active\" "; }?> >Manage Admin</a></li>
+ 	<li><i class="fa fa-hospital-o" aria-hidden="true"></i><a href="/bloodbank/admin/manage_hospital/index" <?php if (strpos($uri, '/admin/manage_hospital')){ echo "class= \"active\" "; }?> >Manage Hospitals</a></li>
+    <li><i class="fa fa-file-text" aria-hidden="true"></i><a href="/bloodbank/admin/view_report/index" <?php if (strpos($uri, '/admin/view_report')){ echo "class= \"active\" "; }?> >View Report</a></li>
+    <li><i class="fa fa-cogs" aria-hidden="true"></i><a href="/bloodbank/admin/profile/index" <?php if (strpos($uri, '/admin/profile')){ echo "class= \"active\" "; }?> >Settings</a></li>
         </div>
     </form>
 </div>
